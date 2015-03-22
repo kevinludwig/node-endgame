01dab2234a723af873a5b79e2d9a42363a6c0378
kevinludwig <kevin.ludwig@gmail.com>
2015-03-21 21:37:15 -0700

    back to navbar-static-top

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,8 @@ module.exports = function(req, res) {
         {fen: '4k3/R7/8/3KP3/8/8/8/7r', stm: 'b'},
         {fen: '8/8/5K2/3p4/3P4/1k6/8/8', stm: 'w'},
         {fen: '3k4/2r5/2B4R/3K4/8/8/8/8', stm: 'w'},
-        {fen: '1k6/8/1K6/P7/3B4/8/8/8', stm: 'w'}
+        {fen: '1k6/8/1K6/P7/3B4/8/8/8', stm: 'w'},
+        {fen: '8/ppp5/8/PPP5/8/6k1/8/6K1', stm: 'w'}
     ];
     res.render('index', {
         results: results.map(util.toObject),
